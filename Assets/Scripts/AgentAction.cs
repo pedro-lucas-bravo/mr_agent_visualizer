367d32947fa729b6921ea41fb98919e92facb8b0
Pedro Lucas <pedro.lucas.bravo@gmail.com>
2022-01-24 23:30:10 +0100

    Add holotoolkit and examples. Add UnityOSC library

---
+++ Assets/Scripts/AgentAction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Academy;
+
+public class AgentAction : InteractibleAction
+{
+    public override void PerformAction() {
+        AgentDataManager.Instance.SelectAgent();
+    }
+
+}
---
+++ Assets/Scripts/AgentDataManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentDataManager : MonoBehaviour
+{
+
+    //public OSC osc;
+    public string addressToSend;
+    public string addressToReceive;
+    public static AgentDataManager Instance;
+    private void Awake() {
+        Instance = this;
+    }
+
+    void Start() {
+        //Sender message
+        //message_ = new OscMessage();
+        //message_.address = addressToSend;
+        //message_.values.Add(0f);
+
+        //Receivers
+        //osc.SetAddressHandler(addressToReceive, OnReceive);
+    }
+
+    public void SelectAgent() {
+        //message_.values[0] = 1.0f;
+        //osc.Send(message_);
+        Debug.LogWarning("OSC SEND");
+    }
+
+    //private void OnReceive(OscMessage oscM) {
+    //    Debug.Log("OSC: " + oscM.GetInt(0));
+    //}
+
+    //OscMessage message_;
+}
