df668d0cb0873d0a27adbeec144e059626cff005
Pedro Lucas <pedro.lucas.bravo@gmail.com>
2022-01-31 13:01:07 +0100

    Implement messages for state communication and use of selection for state change. Receive mocap data and map positions properly.

--- Assets/Scripts/AgentController.cs
+++ Assets/Scripts/AgentController.cs
@@ -4,17 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AgentController : MonoBehaviour, IFocusable, IInputClickHandler {
-    
+
+    public enum State { Locked, Released}
+
     public int Id;
     public Transform trans;
     public Renderer render;
-    public Color onNormalColor;
+    public Color onLockedColor;
+    public Color onReleasedColor;
     public Color onFocusColor;
     public float smoothTime = 0.15f;
+
+    public State state { get; private set; }
 
     void Start() {
         AgentDataManager.Instance.Agents.Add(Id, this);
         lastPosition_ = trans.position;
+        state = State.Locked;
     }
 
     public void OnFocusEnter() {
@@ -22,17 +28,52 @@ public class AgentController : MonoBehaviour, IFocusable, IInputClickHandler {
     }
 
     public void OnFocusExit() {
-        render.material.color = onNormalColor;
+        switch (state) {
+            case State.Locked:
+                ApplyLockedFeedback();
+                break;
+            case State.Released:
+                ApplyReleasedFeedback();
+                break;
+            default:
+                break;
+        }
     }
 
     public void OnInputClicked(InputClickedEventData eventData) {
-        AgentDataManager.Instance.SelectAgent(Id);
+        AgentDataManager.Instance.SelectAgent(this);
     }
 
     #region Data to set
 
     public void SetPosition(Vector3 position) {
         lastPosition_ = position;
+    }
+
+    public void SetState(State newState) {
+        state = newState;
+        switch (newState) {
+            case State.Locked:
+                ApplyLockedFeedback();
+                break;
+            case State.Released:
+                ApplyReleasedFeedback();
+                break;
+            default:
+                break;
+        }
+    }
+
+    #endregion
+
+    #region Feedback
+
+    void ApplyLockedFeedback() {
+        render.material.color = onLockedColor;
+    }
+
+    void ApplyReleasedFeedback() {
+        render.material.color = onReleasedColor;
     }
 
     #endregion
@@ -42,5 +83,5 @@ public class AgentController : MonoBehaviour, IFocusable, IInputClickHandler {
         trans.position = Vector3.SmoothDamp(trans.position, lastPosition_, ref vel, smoothTime);
     }
 
-    private Vector3 lastPosition_;
+    private Vector3 lastPosition_;    
 }
--- Assets/Scripts/AgentDataManager.cs
+++ Assets/Scripts/AgentDataManager.cs
@@ -2,16 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class AgentDataManager : MonoBehaviour
 {    
     public OscManager osc;
 
     [Header("Addresses to send")]
-    public string selectOutAddress;
+    public string selectOutAddress = "/agent/select";
+    public string selectOutState = "/agent/state";
 
     [Header("Addresses to receive")]
-    public string positionInAddress;
+    public string sensorPositionInAddress = "/sensor/position";
+    public string positionInAddress = "/agent/position";
+    public string positionInDawSelect= "/agent/daw/select";
 
     public Dictionary<int, AgentController> Agents { get; set; }
 
@@ -23,7 +27,8 @@ public class AgentDataManager : MonoBehaviour
 
     void Start() {
         //Sender message
-        selectOutMessage_ = osc.DefineMessageToClient(selectOutAddress, 4);
+        selectOutMessage_ = osc.DefineMessageToClient(selectOutAddress, 1);
+        selectOutStateMessage_ = osc.DefineMessageToClient(selectOutState, 2);
 
         //Receivers
         osc.OnReceiveMessage += OnReceive;
@@ -35,26 +40,54 @@ public class AgentDataManager : MonoBehaviour
     }
 
     private void OnReceive(string address, List<object> values) {
-        if (address == positionInAddress) {
+        if (address == sensorPositionInAddress) {//Position from mocap for locked agent
+            var position = new Vector3((int)values[0], (int)values[1], (int)values[2]) / 1000.0f;
+            var lockedAgent = Agents.FirstOrDefault(a => a.Value.state == AgentController.State.Locked);
+            if (lockedAgent.Value != null) {
+                var agent = lockedAgent.Value;
+                agent.SetPosition(position);
+            }
+        }
+
+        if (address == positionInAddress) {//Position for released agent from controller
             var agentId = (int)values[0];
             var position = new Vector3((int)values[1], (int)values[2], (int)values[3]) / 1000.0f;
-            Debug.Log("ID1:" + (int)values[0] + positionInAddress + ": " + position.ToString("F4"));
-            if (Agents.TryGetValue(agentId, out var agent)) {                
+            if (Agents.TryGetValue(agentId, out var agent) && agent.state == AgentController.State.Released) {                
                 agent.SetPosition(position);
-                Debug.Log("ID3:" + (int)values[0] + positionInAddress + ": " + agent.trans.position.ToString("F4"));
-            }            
+                //Debug.Log("ID:" + (int)values[0] + positionInAddress + ": " + agent.trans.position.ToString("F4"));
+            }        
+        }       
+    }
+
+    public void SelectAgent(AgentController agent) {
+
+        //Change state accordingly
+        switch (agent.state) {
+            case AgentController.State.Locked:
+                agent.SetState(AgentController.State.Released);
+                break;
+            case AgentController.State.Released:
+                agent.SetState(AgentController.State.Locked);
+                break;
+            default:
+                break;
+        }
+        
+        //Send Message
+        selectOutStateMessage_[0] = agent.Id;
+        selectOutStateMessage_[1] = (int)agent.state;
+        osc.SendMessageToClient(selectOutState);
+        Debug.LogWarning("OSC SEND");
+
+        //TO DO: Implement confirmation mesages startegy
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.S)) {
+            SelectAgent(Agents[1]);
         }
     }
 
-    public void SelectAgent(int id) {
-        selectOutMessage_[0] = id;
-        selectOutMessage_[1] = 0.254f;
-        selectOutMessage_[2] = 1.236f;
-        selectOutMessage_[3] = 2.857f;
-        
-        osc.SendMessageToClient(selectOutAddress);
-        Debug.LogWarning("OSC SEND");
-    }
-
     List<object> selectOutMessage_;
+    List<object> selectOutStateMessage_;
 }
