775bdeaedda9de65942def5b8a5e18845b13e309
Pedro Lucas <pedro.lucas.bravo@gmail.com>
2022-03-07 15:04:27 +0100

    Implement world reference lock feature

---
+++ Assets/Scripts/WorldReferenceListener.cs
@@ -0,0 +1,26 @@
+﻿using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+using Vuforia;
+
+public class WorldReferenceListener : MonoBehaviour, IInputClickHandler {
+
+    public ImageTargetBehaviour referenceWorldTarget;
+    public Renderer render;
+    public SpriteRenderer arrow;
+    public Color colorOnFree;
+    public Color colorOnLock;
+
+    void Awake() {
+       SetColor(colorOnFree);
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData) {
+        referenceWorldTarget.enabled = !referenceWorldTarget.enabled;
+        SetColor(referenceWorldTarget.enabled ? colorOnFree : colorOnLock);
+    }
+
+    void SetColor(Color color) {
+        render.material.color = color;
+        arrow.color = color;
+    }
+}
