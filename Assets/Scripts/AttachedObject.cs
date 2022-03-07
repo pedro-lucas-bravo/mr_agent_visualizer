8a76ac020c8ed173832dc4709c26881adb4345dc
Pedro Lucas <pedro.lucas.bravo@gmail.com>
2022-03-08 00:09:49 +0100

    Implement screen for showing ip at the beginning of the app, and remove the msg when a connection enters

---
+++ Assets/Scripts/AttachedObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttachedObject : MonoBehaviour
+{
+    public float distanceToHead = 2f;
+    public float smoothTime = 0.15f;
+
+    private void Awake() {
+        trans_ = transform;
+        cameraTrans_ = Camera.main.transform;
+    }
+
+    private void Update() {
+        var targetPosition = cameraTrans_.position + cameraTrans_.forward * distanceToHead;
+        var vel = Vector3.zero;
+        trans_.position = Vector3.SmoothDamp(trans_.position, targetPosition, ref vel, smoothTime);
+        trans_.forward = cameraTrans_.forward;
+        //trans_.up = cameraTrans_.up;
+    }
+
+    Transform trans_;
+    Transform cameraTrans_;
+}
---
+++ Assets/Scripts/ConnectionInfoScreen.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System.Linq;
+using System.Net;
+
+public class ConnectionInfoScreen : MonoBehaviour
+{
+    public TextMeshProUGUI ipLabel;
+    public TextMeshProUGUI portLabel;
+
+    public void Start() {
+        ipLabel.text = GetIp();
+        portLabel.text = "" + OSCHandler.Instance.Servers.First().Value.server.LocalPort;
+    }
+
+#if ENABLE_WINMD_SUPPORT
+    public string GetIp() {
+        List<string> IpAddress = new List<string>();
+        var Hosts = Windows.Networking.Connectivity.NetworkInformation.GetHostNames().ToList();
+        foreach (var Host in Hosts) {
+            string IP = Host.DisplayName;
+            IpAddress.Add(IP);
+        }
+        IPAddress address = IPAddress.Parse(IpAddress.Last());
+        return address.ToString();
+    }
+#else
+    string GetIp() {
+        string localIP;
+        using (System.Net.Sockets.Socket socket = new System.Net.Sockets.Socket(System.Net.Sockets.AddressFamily.InterNetwork, System.Net.Sockets.SocketType.Dgram, 0)) {
+            socket.Connect("8.8.8.8", 65530);
+            System.Net.IPEndPoint endPoint = socket.LocalEndPoint as System.Net.IPEndPoint;
+            localIP = endPoint.Address.ToString();
+        }
+        return localIP;
+    }
+#endif
+
+    private void Update() {
+        //Check if client is connected
+        if (OSCHandler.Instance.Clients.Any()) { //Deactivate object
+            gameObject.SetActive(false);        
+        }
+    }
+
+}
