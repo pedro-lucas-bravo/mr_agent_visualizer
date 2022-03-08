13d16d58b6c815baed24163db0fbe2078808d4c7
Pedro Lucas <pedro.lucas.bravo@gmail.com>
2022-03-08 15:43:16 +0100

    Add graphical feedback to agents regarding fundamental states

--- Assets/Scripts/AgentController.cs
+++ Assets/Scripts/AgentController.cs
@@ -14,22 +14,45 @@ public class AgentController : MonoBehaviour, IFocusable, IInputClickHandler {
     public Color color;
     public TextMeshPro textNumber;
     public TrailRenderer trail;
+    public Renderer cage;
+    public Renderer shell;
     //public Color onLockedColor;
     //public Color onReleasedColor;
     //public Color onFocusColor;
     public float smoothTime = 0.15f;
 
-    public State state { get; private set; }
+    private State state_;
+    public State state {
+        get => state_;
+        private set {
+            state_ = value;
+            switch (state_) {
+                case State.Empty:
+                    ApplyFeedback(true, 0.25f);
+                    break;
+                case State.Locked:
+                    ApplyFeedback(true, 1);
+                    break;
+                case State.Released:
+                    ApplyFeedback(false, 1);
+                    break;
+                default:
+                    break;
+            }
+        } 
+    }
 
     void Awake() {
+        transCore_ = render.transform;
         transText_ = textNumber.transform;
+        transCage_ = cage.transform;
         cameraTrans_ = Camera.main.transform;        
     }
 
     void Start() {
         //AgentDataManager.Instance.Agents.Add(Id, this);
         lastPosition_ = trans.position;
-        //state = State.Locked;
+        state = State.Empty;
     }
 
     public void OnFocusEnter() {
@@ -66,6 +89,7 @@ public class AgentController : MonoBehaviour, IFocusable, IInputClickHandler {
     }
 
     public void SetState(State newState) {
+        if (newState == state) return;
         state = newState;
         switch (newState) {
             case State.Locked:
@@ -80,12 +104,21 @@ public class AgentController : MonoBehaviour, IFocusable, IInputClickHandler {
     }
 
     public void SetStateFromInt(int s) {
-        state = (State)s;
+        SetState((State)s);
     }
 
     public void SetColor(Color c) {
         color = c;
         render.material.color = color;
+
+        var cageColor = color;
+        cageColor.a = cage.material.color.a;
+        cage.material.color = cageColor;
+
+        var shellColor = color;
+        shellColor.a = shell.material.color.a;
+        shell.material.color = shellColor;
+
         var startColor = color;
         startColor.a = 0.5f;
         trail.startColor = startColor;
@@ -104,21 +137,34 @@ public class AgentController : MonoBehaviour, IFocusable, IInputClickHandler {
         //render.material.color = onReleasedColor;
     }
 
+    void ApplyFeedback(bool isLocked, float coreScale) {
+        cage.gameObject.SetActive(isLocked);
+        shell.gameObject.SetActive(!isLocked);
+        transCore_.localScale = coreScale * Vector3.one;
+    }
+
     #endregion
 
     void Update() {
         var vel = Vector3.zero;        
         trans.localPosition = Vector3.SmoothDamp(trans.localPosition, lastPosition_, ref vel, smoothTime);
-        UpdateTextDirection();
+        var dir = (cameraTrans_.position - trans.position).normalized;
+        UpdateTextDirection(dir);
+        UpdateCageDirection(dir);
     }
 
-    void UpdateTextDirection() {
-        var dir = (cameraTrans_.position - trans.position).normalized;
+    void UpdateTextDirection(Vector3 dir) {        
         transText_.position = trans.position + dir * trans.lossyScale.x * 0.5f;
         transText_.forward = -dir;
     }
 
+    void UpdateCageDirection(Vector3 dir) {
+        transCage_.forward = -dir;
+    }
+
     private Vector3 lastPosition_;
+    private Transform transCore_;
     private Transform transText_;
+    private Transform transCage_;
     private Transform cameraTrans_;
 }
