212f71e3f35d870489897b2d348d502ef657d89b
Pedro Lucas <pedro.lucas.bravo@gmail.com>
2022-03-12 13:06:07 +0100

    Improve solution for reference AR

---
+++ Assets/Scripts/TransFollower.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransFollower : MonoBehaviour
+{
+    public Transform target;
+
+    private void Awake() {
+        trans_ = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trans_.position = target.position;
+        trans_.rotation = target.rotation;
+    }
+
+    Transform trans_;
+}
